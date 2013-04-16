688d224db37df0b5e5c2f7d30c35387a35824e97
Nuffic <kaupojuhkam@live.com>
2013-04-16 10:39:54 +0300

    Add request class

--- classes/Request.php
+++ classes/Request.php
@@ -1,9 +1,21 @@
 <?php
 
 class Request {
-	public $controller = REQUEST_CONTROLLER;
+	public $controller;
 	public $action = 'Index';
 	public $params = array();
 
-
+	function __construct()
+	{
+		if (isset($_SERVER['PATH_INFO'])) {
+			if ($path_info = explode('/', $_SERVER['PATH_INFO'])) {
+				array_shift($path_info);
+				$this->controller = isset($path_info[0]) ? array_shift($path_info) : 'welcome';
+				$this->action = isset($path_info[0]) && !empty($path_info[0]) ? array_shift($path_info) : 'index';
+				$this->params = isset($path_info[0]) ? $path_info : null;
+			}
+		}
+	}
 }
+
+$_request = new Request;

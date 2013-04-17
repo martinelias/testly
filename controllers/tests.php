fe5f58124efc64f9c7f147afe298d735beaa6f48
martinelias <martinelias@yohead.com>
2013-04-17 16:23:20 +0300

    Improve master view and add nav bar[fixes #48238383].

--- controllers/tests.php
+++ controllers/tests.php
@@ -11,6 +11,7 @@ class tests {
 	public $requires_auth = TRUE;
 
 	function index(){
-
+		global $request;
+		require 'views/master_view.php';
 	}
 }
--- views/master_view.php
+++ views/master_view.php
@@ -1,9 +1,12 @@
-<?php require'classes/Request.php' ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -15,19 +18,52 @@
 
 	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/normalize.css">
-	<link rel="stylesheet" href="assets/css/main.css" >
+	<link rel="stylesheet" href="assets/css/main.css">
 	<script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
+	<sytle>
+		body(
+		padding-top:60px;
+		)
+	</sytle>
 </head>
 <body>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container">
+			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="brand" href="#">Project name</a>
+
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+					<li class="active"><a href="<?= BASE_URL ?>tests">Home</a></li>
+					<li><a href="#about">About</a></li>
+					<li><a href="<?= BASE_URL ?>auth/logout">Logi v2lja</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+</div>
+
+<div>
+	<?php
+	require 'views/'.$request->controller.'_'.$request->action.'_view.php';
+	?>
+
+</div>
 <!--[if lt IE 7]>
-<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your
+	browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your
+	experience.</p>
 <![endif]-->
 <!-- Add your site or application content here -->
 
 <!-- Add your site or application content here -->
-<?php
-require '/views/controller_action_view.php'
-?>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 <script src="assets/js/plugins.js"></script>

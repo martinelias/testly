c76e2289477f8edebd1d18c71e7d5ed24aeac7f0
martinelias <martinelias@yohead.com>
2013-04-17 11:50:08 +0300

    Add tests controller. Change default controller to tests. Define BASE_URL parameter.

--- config.php
+++ config.php
@@ -3,5 +3,6 @@
 define ('DATABASE_HOSTNAME', 'localhost');
 define ('DATABASE_USERNAME', 'root');
 define ('DATABASE_DATABASE', 'testly');
-define ('DEFAULT_CONTROLLER', 'welcome');
+define ('DEFAULT_CONTROLLER', 'tests');
+define ('BASE_URL', '/testly/');
 ?>
---
+++ controllers/tests.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Yo-Head
+ * Date: 17.04.13
+ * Time: 11:45
+ * To change this template use File | Settings | File Templates.
+ */
+
+class tests {
+	public $requires_auth = TRUE;
+
+	function index(){
+
+	}
+}

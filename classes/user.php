8dbf0759110ce2de4e0460f78c36849f09b2ba3c
martinelias <martinelias@yohead.com>
2013-04-17 11:44:18 +0300

    Add database functions. Add user class. Add auth class. Add master view. Add htaccess. Add config.php. Add readme.

---
+++ classes/user.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Locked
+ * Date: 16.04.13
+ * Time: 13:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+class user
+{
+
+	public $logged_in = FALSE;
+
+	function __construct()
+	{
+		session_start();
+		if (isset($_SESSION['user_id'])) {
+			$this->logged_in = TRUE;
+		}
+	}
+
+	public function require_auth()
+	{
+		global $request;
+		if ($this->logged_in !== TRUE) {
+			if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])
+				&& $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'
+			) {
+				header('HTTP/1.0 401 Unauthorized');
+				exit (json_encode(array('data' => 'session_expired')));
+			} else {
+				$_SESSION['session_expired'] = TRUE;
+				$request->redirect('auth');
+			}
+		}
+	}
+}
+
+$_user = new user;
